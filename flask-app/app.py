a24c524ba728cc12119e4960dbc015073b20b41d
dhathriv25 <dhathri.dha3@gmail.com>
2025-03-04 09:41:08 +0000

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>WELL DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!!!!!!!!!!\n\n NEWWW \n\nUsing Port 8080</h1>'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
